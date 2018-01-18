f4edb15f5e3e6e1a8a6c53e685cd9bbbf9a7c967
GRUPOPLENUM\msoberanis <majesova@gmail.com>
2018-01-18 09:48:53 -0600

    cambis

--- app/javascript/components/actions/ServerActions.js
+++ app/javascript/components/actions/ServerActions.js
@@ -4,7 +4,6 @@ import ActionTypes from "../constants"
 export default {
 	receivedTweets(rawTweets){
 		console.log(3,"ServerActions.receivedTweets");
-
 		AppDispatcher.dispatch({
 			actionType:ActionTypes.RECEIVED_TWEETS,
 			rawTweets
